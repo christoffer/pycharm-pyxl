532f79eca4389426632f9a54f8825ab11fa0d499
Christoffer Klang <christoffer.klang@gmail.com>
2014-07-22 09:15:45 -0700

    Register PyxlTag as an expression
    Fix a bunch of NPEs

---
+++ src/com/christofferklang/pyxl/PyxlDialectTokenContributor.java
@@ -0,0 +1,56 @@
+package com.christofferklang.pyxl;
+
+import com.christofferklang.pyxl.psi.PyxlTag;
+import com.intellij.psi.tree.TokenSet;
+import com.jetbrains.python.PythonDialectsTokenSetContributor;
+import org.jetbrains.annotations.NotNull;
+
+public class PyxlDialectTokenContributor implements PythonDialectsTokenSetContributor {
+    @NotNull
+    @Override
+    public TokenSet getStatementTokens() {
+        return TokenSet.EMPTY;
+    }
+
+    @NotNull
+    @Override
+    public TokenSet getExpressionTokens() {
+        return TokenSet.create(PyxlElementTypes.TAG);
+    }
+
+    @NotNull
+    @Override
+    public TokenSet getNameDefinerTokens() {
+        return TokenSet.EMPTY;
+    }
+
+    @NotNull
+    @Override
+    public TokenSet getKeywordTokens() {
+        return TokenSet.EMPTY;
+    }
+
+    @NotNull
+    @Override
+    public TokenSet getParameterTokens() {
+        return TokenSet.EMPTY;
+    }
+
+    @NotNull
+    @Override
+    public TokenSet getFunctionDeclarationTokens() {
+        return TokenSet.EMPTY;
+    }
+
+    @NotNull
+    @Override
+    public TokenSet getUnbalancedBracesRecoveryTokens() {
+        return TokenSet.EMPTY;
+    }
+
+    @NotNull
+    @Override
+    public TokenSet getReferenceExpressionTokens() {
+        return TokenSet.EMPTY;
+    }
+}
